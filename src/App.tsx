62af7ea204093bd1ed713a4849157a5540b2b773
ray03j <eihouse.potato@outlook.jp>
2023-02-19 20:58:17 +0900

    [add] canvas

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,6 @@ import './App.css';
 import { Canvas } from "@react-three/fiber";
 import * as THREE from "three";
 
-var geometry = new THREE.PlaneGeometry( 150, 150, 64, 64 );
-
-ground = new THREE.Mesh(
-  geometry,
-  new THREE.MeshLambertMaterial( { map: map1 } )
-);
-
 function App() {
   return (
     <div id="canvas-container">

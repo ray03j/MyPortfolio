bd5b7bd383cb2912352feaa12c6147c087b82eb4
ray03j <eihouse.potato@outlook.jp>
2023-02-20 23:44:10 +0900

    [add]4つの立方体の追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
 import React from 'react';
 import './App.css';
 import { Canvas } from "@react-three/fiber";
-import * as THREE from "three";
+import { Cube } from './Components/Cube';
+import { OrbitControls } from '@react-three/drei';
+
 
 function App() {
   return (
     <div id="canvas-container">
-      <Canvas>
-
+      <Canvas
+          camera={{
+            fov: 45,
+            near: 0.1,
+            far: 1000,
+            position: [0, 0, 5]
+          }}
+      >
+          
+          <Cube position={[1, 1, 0]} />
+          <Cube position={[-1, 1, 0]} />
+          <Cube position={[1, -1, 0]} />
+          <Cube position={[-1, -1, 0]} />
+          <spotLight position={[10, 10, 10]} />
+          <ambientLight args={[0xffffff]} intensity={0.2} />
+          <OrbitControls />
+        
       </Canvas>
     </div>
   );
